bfa14b96e29ed07ae30881e6c745963233fe6c45
aito <BoxPistols@users.noreply.github.com>
2023-02-11 21:37:50 +0900

    init

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.containerTitle}>Hello Expo</Text>
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}></View>
+        <View style={styles.rightContainer}></View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+  containerTitle: {
+    color: "orange",
+    fontSize: 32,
+  },
+  itemContainer: { width: "100%" },
+  leftContainer: { width: "100%" },
+  rightContainer: { width: "100%" },
+})
